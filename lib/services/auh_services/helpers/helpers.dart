f29111dd9de679d70b032458cdc36c42514b2987
ousseynoukone <ousseynou781227@gmail.com>
2024-11-22 02:17:24 +0100

    update

--- lib/services/auh_services/helpers/helpers.dart
+++ lib/services/auh_services/helpers/helpers.dart
@@ -3,6 +3,7 @@ import 'package:skilluxfrontendflutter/core/api_service/token_manager.dart';
 import 'package:skilluxfrontendflutter/core/state_managment/app_state_managment.dart';
 import 'package:skilluxfrontendflutter/core/utils/hive_local_storage.dart';
 import 'package:skilluxfrontendflutter/presentations/features/auth/auth.dart';
+import 'package:skilluxfrontendflutter/services/STATE/auth_state/user_state.dart';
 import 'package:skilluxfrontendflutter/services/notification_services/server_side_event/nontification_sse.dart';
 
 Future<void> localLogout() async {
@@ -16,4 +17,5 @@ Future<void> localLogout() async {
   await tokenManager.reinitializeToken();
   notificationSse.disconnecFromTheStream();
   Get.off(() => const Auth());
+  isUserLogginOut.value = false;
 }
